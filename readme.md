25e769a63ac10fdf3f8f537e2c2086573e08308d
Sebastian Allendes <sebastian.allendesortega06@skola.stenungsund.se>
2025-03-18 12:33:42 +0100

    Fyllt loggboken litegrann

--- readme.md
+++ readme.md
@@ -1,7 +1,11 @@
 Min logg
 =============
-
-25-03-11
+februari
 ----------
 
-Idag har jag....
+Jag blev klar med alla delprov och nu ska jag börja på mitt egna projekt
+
+11 mars
+----------
+
+Jag har arbetat med mitt projekt som är ett ord gissnings spel. Jag har en lista med flertal ord som spelet slumpmässigt väljer ut. Spelaren bes om att gissa ett ord som är 5 bokstäver långt sedan. I nuläget har man hur många försök som helst men varje gång du misslyckas ger spelet dig en förolämpning. 
--- test2.py
+++ test2.py
@@ -1,12 +1,17 @@
 import random
 
-ordlista=["groda", "kanin", "råtta","djärv","björn","gädda"]
-insult=["SÅ FACKING DUM"]
+ordlista=["groda", "kanin", "råtta","djärv","björn","gädda","iller", "kossa", "torsk", "bäver"]
+insults=["SÅ FACKING DUM", "Din hjärna har gått på semester och glömt att komma tillbaka", "Det är imponerande hur du alltid lyckas ha fel", "Du får en guldstjärna för försöket… men tyvärr ingen hjärncell på köpet", "Du är beviset på att inte alla utvecklas", "Om jag vill höra något korkat, ringer jag dig"]
+insult_picked=random.choice(insults)
 hemligt_ord=random.choice(ordlista)
 while True: 
     gissning=input("Gissa ordet: ")
     if gissning==hemligt_ord:
         print("SÅ JÄVLA RÄTT")
+        # nytt ord
+        hemligt_ord=random.choice(ordlista)
     else: 
-        print("SÅ FACKING DUM")
+        insult_picked=random.choice(insults)
+        print(insult_picked)
+        # samma ord
 
